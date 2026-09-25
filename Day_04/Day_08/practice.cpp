e050d3994cc7b1350845a63ec4298d52d84baf82
kainat4545 <f2025-0999@bnu.edu.pk>
2026-09-25 19:35:54 +0500

    practice the questions

---
+++ Day_04/Day_08/practice.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string s = "pakistan";
+
+    cout << s[0] << endl;
+    cout << s[1] << endl;
+    cout << s[2]<< endl;
+    cout << s[3] << endl;
+    cout << s[4] << endl;
+    cout << s[5] << endl;
+    cout << s[6] << endl;
+    cout << s[7] << endl;
+    cout<<endl;
+
+
+    char array[8] = {'p', 'a', 'k', 'i', 's', 't', 'a', 'n'};
+    cout << array[0] << endl;
+    cout << array[1] << endl;
+    cout << array[2] << endl;
+    cout << array[3] << endl;
+    cout << array[4] << endl;
+    cout << array[5] << endl;
+    cout << array[6] << endl;
+    cout << array[7] << endl;
+
+    return 0;
+}
